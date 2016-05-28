68689984bf4af1f38d403e4e92ef7818eb9feabe
Bjorn Bouet Smith <bjornsmith@gmail.com>
2016-05-28 14:49:04 +0200

    Initial .NET 4.5 solution structure

---
+++ src/ExpSer/Public/ILogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExpSer.Public
+{
+    /// <summary>
+    /// Interface for logging purposes within the ExpSer framework.
+    /// Users of ExpSer can implement an instance of this if they want
+    /// to see the emitted log messages of ExpSer.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs a message at "Trace" level
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        void Trace(string message);
+
+        /// <summary>
+        /// Logs a message at "Info" level
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        void Info(string message);
+
+        /// <summary>
+        /// Logs a message at "Warn" level
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        void Warn(string message);
+
+        /// <summary>
+        /// Logs a message at "Error" level
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        void Error(string message);
+
+        /// <summary>
+        /// Logs a message at "Error" level
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">The exception that happened.</param>
+        void Error(string message, Exception exception);
+
+    }
+}
---
+++ src/ExpSer/Public/ISerializer.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpSer.Public
+{
+    /// <summary>
+    /// Serializer interface for serialization and deserialization of ExpSer objects.
+    /// </summary>
+    public interface ISerializer
+    {
+
+
+    }
+}
