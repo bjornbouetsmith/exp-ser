5e8876748b78e1cf59e86325c5c72d515549e9ad
Bjorn Bouet Smith <bjornsmith@gmail.com>
2016-05-28 15:40:04 +0200

    Added nuget + package restore to build. Added null logger for when no logger is provided

---
+++ src/ExpSer/Public/NullLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExpSer.Public
+{
+    internal class NullLogger : ILogger
+    {
+        public void Trace(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Warn(string message)
+        {
+        }
+
+        public void Error(string message)
+        {
+        }
+
+        public void Error(string message, Exception exception)
+        {
+        }
+    }
+}
